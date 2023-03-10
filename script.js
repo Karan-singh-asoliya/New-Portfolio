ab0984f2485e7cc373b359b36e002e7113d4a137
DinAnsh <54740603+DinAnsh@users.noreply.github.com>
2023-03-10 19:16:25 +0530

    developed contact and nav with js

--- script.js
+++ script.js
@@ -1,29 +1,48 @@
-$(window).scroll(function() {
-  
-    // selectors
-    var $window = $(window),
-        $body = $('body'),
-        $panel = $('.panel');
-    
-    // Change 33% earlier than scroll position so colour is there when you arrive.
-    var scroll = $window.scrollTop() + ($window.height() / 3);
-   
-    $panel.each(function () {
-      var $this = $(this);
+//--------------navigation bar color change on scroll-----------------
+
+function scrollValue() {
+  var navbar = document.getElementById("navbar");
+  var scroll = window.scrollY;
+  if (scroll < 500) {
+    navbar.classList.remove("BgColour");
+  } else {
+    navbar.classList.add("BgColour");
+  }
+}
+window.addEventListener("scroll", scrollValue);
+
+
+// ----------------------- toggle contact button---------------
+
+document.addEventListener("DOMContentLoaded", function () {
+  const showFormBtn = document.getElementById('show-form-btn');
+  const myForm = document.getElementById('contact');
+  var footer = document.getElementById('footer');
+  const btns = document.querySelectorAll('a');
+  const btn = btns[btns.length - 1];
+
+  showFormBtn.addEventListener('click', function () {
+    if (myForm.style.display === 'none') {
+      myForm.style.display = 'block';
       
-      // if position is within range of this panel.
-      // So position of (position of top of div <= scroll position) && (position of bottom of div > scroll position).
-      // Remember we set the scroll to 33% earlier in scroll var.
-      if ($this.position().top <= scroll && $this.position().top + $this.height() > scroll) {
-            
-        // Remove all classes on body with color-
-        $body.removeClass(function (index, css) {
-          return (css.match (/(^|\s)color-\S+/g) || []).join(' ');
-        });
-         
-        // Add class of currently active div
-        $body.addClass('color-' + $(this).data('color'));
-      }
-    });    
-    
-  }).scroll();
+      window.scrollTo({
+        top: document.body.scrollHeight,
+        behavior: 'smooth'
+      });
+      
+      footer.style.color = '#d6d6d6';
+    } else {
+      myForm.style.display = 'none';
+      footer.style.color = '#0b0909';
+    }
+    if (btn) {
+      btn.addEventListener('mouseover', function () {
+        this.style.boxShadow = '0 0 20px 2px #d6d6d6';
+      });
+      btn.addEventListener('mouseout', function() {
+        this.style.boxShadow = '';
+      });
+    }
+  });
+
+});
