e5e2bc0baad4390abd31ab781fc99520000584d9
DinAnsh <54740603+DinAnsh@users.noreply.github.com>
2023-03-11 01:15:26 +0530

    improved image-loading and completed the 'contact'

--- script.js
+++ script.js
@@ -14,35 +14,35 @@ window.addEventListener("scroll", scrollValue);
 
 // ----------------------- toggle contact button---------------
 
-document.addEventListener("DOMContentLoaded", function () {
-  const showFormBtn = document.getElementById('show-form-btn');
-  const myForm = document.getElementById('contact');
-  var footer = document.getElementById('footer');
-  const btns = document.querySelectorAll('a');
-  const btn = btns[btns.length - 1];
+// document.addEventListener("DOMContentLoaded", function () {
+//   const showFormBtn = document.getElementById('show-form-btn');
+//   const myForm = document.getElementById('contact');
+//   var footer = document.getElementById('footer');
+//   const btns = document.querySelectorAll('a');
+//   const btn = btns[btns.length - 1];
 
-  showFormBtn.addEventListener('click', function () {
-    if (myForm.style.display === 'none') {
-      myForm.style.display = 'block';
+//   showFormBtn.addEventListener('click', function () {
+//     if (myForm.style.display === 'none') {
+//       myForm.style.display = 'block';
       
-      window.scrollTo({
-        top: document.body.scrollHeight,
-        behavior: 'smooth'
-      });
+//       window.scrollTo({
+//         top: document.body.scrollHeight,
+//         behavior: 'smooth'
+//       });
       
-      footer.style.color = '#d6d6d6';
-    } else {
-      myForm.style.display = 'none';
-      footer.style.color = '#0b0909';
-    }
-    if (btn) {
-      btn.addEventListener('mouseover', function () {
-        this.style.boxShadow = '0 0 20px 2px #d6d6d6';
-      });
-      btn.addEventListener('mouseout', function() {
-        this.style.boxShadow = '';
-      });
-    }
-  });
+//       footer.style.color = '#d6d6d6';
+//     } else {
+//       myForm.style.display = 'none';
+//       footer.style.color = '#0b0909';
+//     }
+//     if (btn) {
+//       btn.addEventListener('mouseover', function () {
+//         this.style.boxShadow = '0 0 20px 2px #d6d6d6';
+//       });
+//       btn.addEventListener('mouseout', function() {
+//         this.style.boxShadow = '';
+//       });
+//     }
+//   });
 
-});
+// });
